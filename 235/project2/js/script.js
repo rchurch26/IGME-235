108cd9fa0f5e73b96b3c45d8227858d86e98ace8
rchurch26 <rahmelchurch@gmail.com>
2022-04-15 23:57:28 -0400

    A buggy mess of project 2

--- 235/project2/js/script.js
+++ 235/project2/js/script.js
@@ -1,4 +1,5 @@
 window.onload = (e) => document.querySelector("button").onclick = quizButtonClicked;
+let answers = [];
 function getData(url)
 {
     let xhr = new XMLHttpRequest();
@@ -9,8 +10,24 @@ function getData(url)
 }
 function quizButtonClicked()
 {
-    const triviaUrl = "https://opentdb.com/api.php?amount=50";
+    let difficulty = difficultyChosen();
+    let category = categoryChosen();
+    let numQuestions = document.querySelector("#numQuestions").value;
+    const triviaUrl = "https://opentdb.com/api.php?";
     let url = triviaUrl;
+    if(difficulty == "any" && category == "any")
+    {
+        url += `amount=${numQuestions}`;
+    }else if(category == "any")
+    {
+        url += `amount=${numQuestions}&difficulty=${difficulty}`;
+    }else if(difficulty == "any")
+    {
+        url += `amount=${numQuestions}&category=${category}`;
+    }else
+    {
+        url += `amount=${numQuestions}&category=${category}&difficulty=${difficulty}`;
+    }
     console.log(url);
     getData(url);
 }
@@ -19,6 +36,23 @@ function dataLoaded(e)
     let xhr = e.target;
     let obj = JSON.parse(xhr.responseText);
     let questions = obj.results;
+    let question = questions[0];
+    document.querySelector("#questions").innerHTML = `<p>${question.category}<br>${question.question}<br>${question.difficulty}</p>`;
+    answers.push(question.correct_answer);
+    for(let i = 0; i < question.incorrect_answers.length; i++)
+    {
+        answers.push(question.incorrect_answers[i]);
+    }
+    answers.sort();
+    for(let i = 0; i < answers.length; i++)
+    {
+        document.querySelector("#answers").innerHTML += `<p><input type='radio' name='answers' value='${answers[i]}'>${answers[i]}</p>`;
+    }
+    let choice = document.querySelector("#answers[name]");
+    if(choice.value == question.correct_answer)
+    {
+        document.querySelector("#result").innerHTML = `Correct`;
+    }
 }
 function dataError(e)
 {
@@ -40,11 +74,26 @@ function difficultyChosen()
 function categoryChosen()
 {
     let categories = document.querySelectorAll(".category");
-    for(let c of categories)
+    for(let i = 0; i < categories.length; i++)
     {
-        if(c.selected == true)
+        if(categories[i].selected == true)
         {
-            return c.value;
+            if(categories[i].value == "any")
+            {
+                return "any";
+            }
+            return i + 8;
+        }
+    }
+}
+//checkAnswer Function
+function checkAnswer()
+{
+    for(let a of answers)
+    {
+        if(a.selected == true)
+        {
+            return a;
         }
     }
 }
